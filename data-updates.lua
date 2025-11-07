8aa5d6991f5dd4b5c39c18adeb241fa9c3880e1f
gamma-delta <petrathekat@gmail.com>
2025-11-07 23:25:16 +0100

    Fix fracking modules going in every machine, dingus

--- data-updates.lua
+++ data-updates.lua
@@ -6,11 +6,38 @@
 -- temperatures if you add and remove this mod.
 data.raw["fluid"]["lubricant"].fuel_value = "1kJ"
 
-for _,drill in pairs(data.raw["mining-drill"]) do
-  if drill.allowed_module_categories then
-    table.insert(drill.allowed_module_categories, "pk-fracking-modules")
+-- AUGHH URGH ARUGH
+local all_module_categories = {}
+local amc_but_fracking = {}
+for name,_ in pairs(data.raw["module-category"]) do
+  table.insert(all_module_categories, name)
+  if name ~= "pk-fracking-modules" then
+    table.insert(amc_but_fracking, name)
   end
-  -- If there's no allowed modules, don't add fracking
+end
+print(serpent.line(amc_but_fracking))
+
+for _,drill in pairs(data.raw["mining-drill"]) do
+  if drill.effect_reciever then
+    if drill.allowed_module_categories then
+      -- TODO: a blacklist for developers?
+      table.insert(drill.allowed_module_categories, "pk-fracking-modules")
+    else
+      drill.allowed_module_categories = all_module_categories
+    end
+  end
+end
+for _,proto_type in ipairs{
+  "beacon", "lab",
+  "assembling-machine", "rocket-silo", "furnace",
+  -- Mining drills handled above
+} do
+  for _,proto in pairs(data.raw[proto_type]) do
+    -- Dont modify if it has a custom AMC
+    if not proto.allowed_module_categories then
+      proto.allowed_module_categories = amc_but_fracking
+    end
+  end
 end
 
 for level=1,4 do
